449d9c0c6ca726bd665f4fec15bb795af33559cc
Anton Alley <Anton.Alley@gmail.com>
2023-04-04 10:18:42 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Add smooth scrolling to navigation links
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+  e.preventDefault();
+  document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+  });
+});
